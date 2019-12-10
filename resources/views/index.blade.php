e11f12f49767a2f4d0c8186c3df4707527f2a63b
Nikola <nidza.bg@gmail.com>
2019-12-10 02:11:23 +0100

    Fixed a bug with Comments controller.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,13 @@
 @extends("layout.main")
 @section("body")
-
+@component("components.card")
+    @slot("heading") Welcome @endslot
     <p>Welcome to the main page. This is a guestbook with authentication.</p>
     @auth
         <p>We can see you are logged in {{ Auth::user()->name }}. Welcome. You have full access.</p>
     @endauth
     @guest
-        <p>You are not logged in. Please log in or register in order to comment.</p>
+        <p>You are not logged in. Please <a href="/login">log in</a> or <a href="/register">register</a> in order to comment.</p>
     @endauth
-
+@endcomponent
     @endsection
