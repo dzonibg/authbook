a073496d601d47136a2e4bb0ad93437311a905c8
Nikola <nidza.bg@gmail.com>
2019-12-12 21:48:48 +0100

    Added comments view.

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -14,7 +14,8 @@ class CommentController extends Controller
      */
     public function index()
     {
-        //
+        $comments = Comment::all();
+        return view("comment.index", compact("comments"));
     }
 
     /**
--- resources/views/comment/index.blade.php
+++ resources/views/comment/index.blade.php
@@ -1,6 +1,13 @@
 @extends("layout.main")
 @section("body")
 
-
-
+<p>Comments</p>
+    @foreach($comments as $comment)
+    @component("components.card")
+    @slot("heading") {{$comment->user->name}} @endslot
+    @slot("slot")
+    {{$comment->text}}
+    @endslot
+    @endcomponent
+    @endforeach
     @endsection
