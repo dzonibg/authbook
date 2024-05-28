1e3f3553e6afac9fa3ff14ab5fed07641209128b
Nikola Stojisavljević <33302164+dzonibg@users.noreply.github.com>
2024-05-29 00:55:25 +0200

    Minor changes for deployment purposes.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,7 @@
 @extends("layout.main")
 @section("body")
 @component("components.card")
-    @slot("heading") Welcome @endslot
+    @slot("heading") Welcome! @endslot
     <p>Welcome to the main page. This is a guestbook with authentication.</p>
     @auth
         <p>We can see you are logged in {{ Auth::user()->name }}. Welcome. You have full access.</p>
